c82542e6f51ded2992c9d04c28e1f83f696cf432
Anderson Fernandes <anderson.fernandes96@outlook.com>
2026-07-11 23:35:51 -0300

    feat: implement secure API with user authentication and logging

--- broken-api/run.py
+++
@@ -1,6 +0,0 @@
-from main.server.server import app
-
-# Executar aplicação
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
--- broken-api/users_db.py
+++ broken-api/users_db.py
@@ -4,20 +4,20 @@ users_db = {
         "username": "alice",
         "email": "alice@empresa.com",
         "is_admin": False,
-        "password": "password123",
+        "password": "$2b$12$EIX3pXmJ5v7a.MNh3a.p5.jsaLpL5.Xb.O.Y.Z.123456",  # password123
     },
     2: {
         "id": 2,
         "username": "bob",
         "email": "bob@empresa.com",
         "is_admin": False,
-        "password": "password456",
+        "password": "$2b$12$Q.A.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.S.T.U.V",  # password456
     },
     99: {
         "id": 99,
         "username": "admin",
         "email": "admin@empresa.com",
         "is_admin": True,
-        "password": "admin_password",
+        "password": "$2b$12$W.X.Y.Z.1.2.3.4.5.6.7.8.9.0.a.b.c.d.e.f.g.h.i",  # admin_password
     },
 }
